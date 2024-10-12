09b019d78f85f23bae85d8ce0bdcf099eb0f9836
clencyc <clency2023@gmail.com>
2024-10-12 20:23:57 +0300

    Updated the Dao

--- app/src/main/java/com/hutsy4/jetnote/data/NotesDao.kt
+++ app/src/main/java/com/hutsy4/jetnote/data/NotesDao.kt
@@ -1,4 +1,23 @@
 package com.hutsy4.jetnote.data
 
-class NotesDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NotesDao {
+
+    // Insert a new note
+    @Upsert
+    suspend fun upsertNotes(note: Note): Long
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Query("SELECT * FROM notes ORDER BY createdDate DESC")
+    fun getNotes(): Flow<List<Note>>
+
+
 }
