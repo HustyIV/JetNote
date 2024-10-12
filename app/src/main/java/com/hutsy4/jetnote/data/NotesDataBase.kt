eb4e536867c77c6ca7d8741e56a5c38a7c3badcd
clencyc <clency2023@gmail.com>
2024-10-12 20:24:21 +0300

    setup the database

--- app/src/main/java/com/hutsy4/jetnote/data/NotesDataBase.kt
+++ app/src/main/java/com/hutsy4/jetnote/data/NotesDataBase.kt
@@ -1,4 +1,13 @@
 package com.hutsy4.jetnote.data
 
-class NotesDataBase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [Note::class],
+    version = 1,
+)
+abstract class NotesDataBase: RoomDatabase() {
+
+    abstract val dao: NotesDao
 }
